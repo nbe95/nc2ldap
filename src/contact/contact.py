49af2e0da8058c1cc5877a5b649393a9d4bae2be
Niklas Bettgen <niklas.bettgen@bertrandt-development.com>
2023-10-16 11:09:58 +0200

    Add log error for debugging

--- src/contact/contact.py
+++ src/contact/contact.py
@@ -1,9 +1,14 @@
 """Module for system-independent contact management."""
 
-from dataclasses import dataclass
+import logging
+from dataclasses import asdict, dataclass
+from os import environ as env
 from typing import Optional, Tuple
 
 from phonenumbers import FrozenPhoneNumber
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG if env.get("DEBUG", "") else logging.INFO)
 
 
 @dataclass(frozen=True, eq=True)
@@ -23,9 +28,16 @@ class Contact:
 
     def get_cn(self) -> str:
         """Build a CN based on this contact's data (full name or company)."""
-        return " ".join(
+        result: str = " ".join(
             (v or "" for v in (self.title, self.first_name, self.last_name))
         ).strip()
+        if not result:
+            logger.error(
+                "Could not retreive a CN for %s with data fields: %s",
+                self,
+                asdict(self),
+            )
+        return result
 
     def __repr__(self) -> str:
         """Generate a serialized representation for nice log output."""
