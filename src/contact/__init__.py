eb92494d30cbd8f7ad6677dc4faec64d05a26129
Niklas Bettgen <niklas@bettgen.de>
2024-05-14 12:29:15 +0200

    Line length 100

--- src/contact/__init__.py
+++ src/contact/__init__.py
@@ -1,11 +1,7 @@
 """Main module for contact management."""
 
 from .contact import Contact
-from .converters import (
-    contact_from_ldap_dict,
-    contact_from_vcard,
-    contact_to_ldap_dict,
-)
+from .converters import contact_from_ldap_dict, contact_from_vcard, contact_to_ldap_dict
 
 __ALL__ = (
     Contact,
